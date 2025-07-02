daaf6d39f55fb2ae63c7145a4d2b172d00f80054
Priyanku Saikia <123909182+Priyanku-AI@users.noreply.github.com>
2025-07-02 08:08:51 +0530

    Update HeroSection video button position and style
    Changed the video play button from absolute to fixed positioning, adjusted its location, and updated background opacity. Added a 'Watch Video' label next to the button for improved clarity.

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -20,16 +20,11 @@ function HeroSection() {
           placeholder="Search for destinations, experiences, or packages.."
           className="w-1/2 pl-8 pr-2 py-2 mt-4 ml-90 rounded-full bg-white/20 backdrop-blur-md border border-blue-500 text-white placeholder:text-white focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300 shadow-[0_0_10px_#3b82f6,0_0_20px_#3b82f6]"
         />
-
-        {/* <input
-          type="text"
-          placeholder="Search for destinations, experiences, or packages..."
-          className="w-1/2 p-3 mt-4 ml-90 rounded-md bg-white/20 backdrop-blur-md border border-white/20 text-white placeholder:text-white focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300"
-        /> */}
       </div>
-      <div className="absolute left-0 rounded-full w-14 h-14 border border-white bg-white/20 flex items-center justify-center mt-6"> 
+      <div className="fixed right-130 top-59 rounded-full w-14 h-14 border border-white bg-white/5 flex items-center justify-center mt-6">
         <FontAwesomeIcon icon={faPlay} className="text-white text-xl" />
-      </div> 
+        <span className="text-white text-sm font-bold ml-2 fixed right-106 top-70">Watch Video</span>
+      </div>
     </div>
   );
 }
